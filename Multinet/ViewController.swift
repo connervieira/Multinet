87fb655cfc2755ef94fa5f96935753a72250581b
Conner Vieira <vieirast@icloud.com>
2019-06-12 19:51:16 -0400

    Initial Commit

---
+++ Multinet/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Multinet
+//
+//  Created by Conner Vieira on 6/12/19.
+//  Copyright © 2019 V0LT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
